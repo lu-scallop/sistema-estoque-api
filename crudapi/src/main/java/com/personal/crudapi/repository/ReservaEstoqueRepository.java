7d41c41c3c1d9db0a90532b270661296f0c4b4da
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-08-25 11:54:41 -0300

    Adicionando método para retornar saldo disponivel em ReservaEstoque

--- crudapi/src/main/java/com/personal/crudapi/repository/ReservaEstoqueRepository.java
+++ crudapi/src/main/java/com/personal/crudapi/repository/ReservaEstoqueRepository.java
@@ -1,9 +1,27 @@
 package com.personal.crudapi.repository;
 
+import com.personal.crudapi.entity.CentroCusto;
+import com.personal.crudapi.entity.Material;
 import com.personal.crudapi.entity.ReservaEstoque;
+import com.personal.crudapi.enums.StatusReserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface ReservaEstoqueRepository extends JpaRepository<ReservaEstoque, Long> {
+    @Query("SELECT COALESCE(SUM(r.quantidade), 0 " +
+            "FROM ReservaEstoque r " +
+            "WHERE r.material = :material " +
+            "AND r.origem = :origem " +
+            "AND r.status IN :status")
+
+    Long reservadoEmAberto (@Param("material") Material material,
+                            @Param("origem")CentroCusto origem,
+                            @Param("statuses")Collection<StatusReserva> statuses);
+
+    List<ReservaEstoque> findByStatus(StatusReserva status);
 }
--- crudapi/src/main/java/com/personal/crudapi/service/ReservaEstoqueService.java
+++ crudapi/src/main/java/com/personal/crudapi/service/ReservaEstoqueService.java
@@ -1,6 +1,8 @@
 package com.personal.crudapi.service;
 
 import com.personal.crudapi.dto.ReservaEstoqueDTO;
+import com.personal.crudapi.entity.CentroCusto;
+import com.personal.crudapi.entity.Material;
 import com.personal.crudapi.entity.ReservaEstoque;
 import com.personal.crudapi.enums.StatusReserva;
 import com.personal.crudapi.repository.ReservaEstoqueRepository;
@@ -16,10 +18,22 @@ public class ReservaEstoqueService {
 
     @Autowired
     private ReservaEstoqueRepository repository;
+    @Autowired
+    private EstoqueCentroCustoService estoqueService;
 
     @Transactional
     public ReservaEstoque adicionaReserva(ReservaEstoqueDTO dto){
         ReservaEstoque reserva = new ReservaEstoque();
+
+        if (dto.getMaterial() == null)
+            throw new IllegalArgumentException("Material não encontrado.");
+        if (dto.getCentroCustoOrigem() == null)
+            throw new IllegalArgumentException("Centro de origem não encontrado.");
+        if (dto.getCentroCustoDestino() == null)
+            throw new IllegalArgumentException("Centro de destino não encontrado.");
+        if (dto.getCentroCustoOrigem().equals(dto.getCentroCustoDestino()))
+            throw new IllegalArgumentException("Origem e Destino NÃO devem ser iguais.");
+
         reserva.setMaterial(dto.getMaterial());
         reserva.setCentroCustoOrigem(dto.getCentroCustoOrigem());
         reserva.setCentroCustoDestino(dto.getCentroCustoDestino());
@@ -29,6 +43,15 @@ public class ReservaEstoqueService {
         return repository.save(reserva);
 
     }
+
+    @Transactional
+    public Long salvoDisponivelParaReserva(Material material, CentroCusto origem)
+    {
+        Long saldo = estoqueService.adicionaOuObtem(material, origem).getSaldo();
+        Long reservado = repository.reservadoEmAberto(material, origem, List.of(StatusReserva.APROVADA, StatusReserva.ABERTA));
+        return saldo - reservado;
+    }
+
 
     public List<ReservaEstoque> listaTodasAsReservas() { return repository.findAll(); }
 }
