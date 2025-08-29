976d5a3ef255f98787a998dde29f25556304c907
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-08-29 11:11:19 -0300

    Adicionado método criar no ReservaEstoqueController

--- crudapi/src/main/java/com/personal/crudapi/controller/ReservaEstoqueController.java
+++ crudapi/src/main/java/com/personal/crudapi/controller/ReservaEstoqueController.java
@@ -1,8 +1,12 @@
 package com.personal.crudapi.controller;
 
+import com.personal.crudapi.dto.ReservaEstoqueDTO;
 import com.personal.crudapi.entity.ReservaEstoque;
 import com.personal.crudapi.service.ReservaEstoqueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +17,10 @@ public class ReservaEstoqueController {
     @Autowired
     private ReservaEstoqueService service;
 
-
+    @PostMapping
+    public ResponseEntity<ReservaEstoque> criar(@RequestBody ReservaEstoqueDTO dto){
+        return ResponseEntity.ok(service.criaReserva(dto));
+    }
 
 
 }
